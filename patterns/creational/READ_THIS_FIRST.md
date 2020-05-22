e42c4d8450a6b781971ffc0a6e986921c394f204
Vikram Iyer <vikram@acceldata.io>
2020-05-22 21:50:57 +0530

    code: factory pattern refined, readme updated

--- patterns/creational/READ_THIS_FIRST.md
+++ patterns/creational/READ_THIS_FIRST.md
@@ -6,7 +6,11 @@ Ex: Suppose we have 2 ml models for classification; RandomForest and NaiveBayes,
 In such a case, we can have a Factory Class/Method that can check the keyword and invoke the corresponding class (ml model)
 > Corresponding code file: [factory.py](factory.py) in the same directory.
 
-### Abstract Factory
+Since the factory class helps us create other objects, it is a creational pattern.
+
+If you need a more comprehensive example, visit this blog.
+
+https://realpython.com/factory-method-python/
 
 ### Singleton
 
--- patterns/creational/factory.py
+++ patterns/creational/factory.py
@@ -1,12 +1,53 @@
-class NaiveBayesModel(**params):
-    pass
+from abc import ABCMeta, abstractmethod
+from sklearn.naive_bayes import GaussianNB
 
-class RandomForestClassifierModel(**params):
-    pass
+class IModel(metaclass=ABCMeta):
+    """The Model Interface"""
 
-def get_model(model_name='naive_bayes', params=None):
-    models = dict(
-        naive_bayes=NaiveBayesModel(**params),
-        random_forest=RandomForestClassifierModel(**params)
-    )
-    return models[model_name]
+    @abstractmethod
+    def fit(self, X, y):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def predict(self, X):
+        raise NotImplementedError()
+
+class NaiveBayesModel(IModel):
+
+    def __init__(self, **params):
+        self.params = **params
+        self.model = None
+
+    def fit(self, X, y):
+        self.nb = GaussianNB(**self.params)
+        self.nb.fit(X, y)
+
+    def predict(self, X):
+        return self.nb.predict(X)
+
+class RandomForestClassifierModel(IModel):
+
+    def fit(self, X, y):
+        raise NotImplementedError()
+
+    def predict(self, X):
+        raise NotImplementedError()
+
+class ModelFactory:
+    def get_model(model_name='naive_bayes', **params):
+        models = dict(
+            naive_bayes=NaiveBayesModel(params),
+            random_forest=RandomForestClassifierModel(params)
+        )
+        return models[model_name]
+
+if __name__ == '__main__':
+    factory = ModelFactory()
+    p = dict(n_classes=[.22, .54, .....], var_smoothing=1e-9)
+    X = [1, 2, 3, 4, 5, ...]
+    y = [1, 0, 0, 1, 0, ...]
+    X_test = [11, 90]
+    model = factory.get_model('random_forest', p)
+    model.fit(X, y)
+    predictions = model.predict(X_test)
+    print(predictions)
